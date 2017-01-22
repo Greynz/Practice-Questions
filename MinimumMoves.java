9d0b369558afcaba5d31e318a94c7d8157cf46a3
root <dr.dangers@gmail.com>
2017-01-22 00:17:08 -0800

    -changed Minimum Moves approach from adding +1 to all non maximum (or maximum if multiples) to getting the difference between all elements and min.

--- MinimumMoves.java
+++ MinimumMoves.java
@@ -7,12 +7,17 @@ public class MinimumMoves {
     }
 
     public int minMoves(int[] nums) {
-        int max = nums[0];
+        int min = nums[0];
+        int count = 0;
         for (int i=1; i<nums.length; i++){
-            if (nums[i] > max)
-                max = nums[i];
+            if (nums[i] < min)
+                min = nums[i];
         }
-        System.out.println(max);
-        return total - (total/nums.length);
+
+        for (int i=0; i<nums.length; i++){
+            count += nums[i] - min;
+        }
+
+        return count;
     }
 }
